e7bf433f51745ebb52e7f0ca6024f5d0dd42de36
Kayevo <kayevo@protonmail.com>
2022-04-22 15:30:30 -0300

    Feat (controller): create recyclerView adapter controller.

--- app/src/main/java/com/natankayevo/mysubscribers/MainActivity.kt
+++ app/src/main/java/com/natankayevo/mysubscribers/MainActivity.kt
@@ -6,16 +6,25 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.natankayevo.mysubscribers.databinding.ActivityMainBinding
+import com.natankayevo.mysubscribers.databinding.SubscriberItemBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var binding2: SubscriberItemBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
+
+        //
+        binding2 = SubscriberItemBinding.inflate(layoutInflater)
+        val view2 = binding2.root
+
+        binding2.
+        //
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
                 as NavHostFragment
---
+++ app/src/main/java/com/natankayevo/mysubscribers/ui/subscriberlist/SubscriberListAdapter.kt
@@ -0,0 +1,50 @@
+package com.natankayevo.mysubscribers.ui.subscriberlist
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.natankayevo.mysubscribers.data.database.entity.SubscriberEntity
+import com.natankayevo.mysubscribers.databinding.SubscriberItemBinding
+
+class SubscriberListAdapter(
+    private val subscriberList: List<SubscriberEntity>
+) : RecyclerView.Adapter<SubscriberListAdapter.SubscriberListViewHolder>() {
+
+    // inside the onCreateViewHolder inflate the view of SubscriberItemBinding
+    // and return new ViewHolder object containing this layout
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SubscriberListViewHolder {
+        val view = SubscriberItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false)
+
+        return SubscriberListViewHolder(view)
+    }
+
+    // bind the items with each item
+    // of the list subscriberList
+    // which than will be
+    // shown in recycler view
+    override fun onBindViewHolder(holder: SubscriberListViewHolder, position: Int) {
+        holder.bindView(subscriberList[position])
+    }
+
+    override fun getItemCount(): Int = subscriberList.size
+
+    // create an inner class with name SubscriberListViewHolder
+    // It takes a view argument, in which pass the generated class of subscriber_item.xml
+    inner class SubscriberListViewHolder(val binding: SubscriberItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        private val txtViewName: TextView = binding.textSubscriberName
+        private val txtViewEmail: TextView = binding.textSubscriberEmail
+
+        fun bindView(subscriber: SubscriberEntity) {
+            txtViewName.text = subscriber.name
+            txtViewEmail.text = subscriber.email
+
+        }
+    }
+
+}
